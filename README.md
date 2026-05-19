e7c9f96f0f241fcaa33fc765c876444c77f6b59e
nanthamom <nantha.mom@city.ac.uk>
2026-05-19 20:48:25 +0100

    Initial PocketFlow Setup With Expense Tracker

---
+++ README.md
@@ -0,0 +1,50 @@
+# PocketFlow 
+
+PocketFlow Is A Mobile Expense Tracking App built With React Native And Expo While Learning Mobile Development from Scratch.
+
+## Features (Current)
+
+- Add expenses  
+- Dynamic expense list  
+- Automatic total spending calculation  
+- iOS Simulator support  
+- React Native + Expo setup  
+
+Example:
+
+Tesco — £10  
+Waitrose — £12.99  
+Coffee — £4.50  
+
+Total: £27.49
+
+## Tech Stack
+
+- React Native
+- Expo
+- TypeScript
+- Xcode Simulator (iOS)
+- useState
+- Arrays (`map`, `reduce`)
+
+## What I Learned
+
+- Setting up Expo and Xcode
+- Running iOS simulators
+- React state (`useState`)
+- Dynamic rendering with `.map()`
+- Calculating totals with `.reduce()`
+- Debugging React Native and simulator errors
+
+## Future Features
+
+- Delete expenses
+- Categories (Food, Transport, Shopping)
+- Monthly charts
+- Persistent storage
+- AI spending insights
+- Backend integration
+
+## Status (In Progress...)
+
+Learning Project — Actively Building
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+
+export default function HomeScreen() {
+
+  const [amount, setAmount] = useState("");
+  const [note, setNote] = useState("");
+
+  const [expenses, setExpenses] =
+    useState<string[]>([]);
+
+  function addExpense() {
+
+    const newExpense =
+      `${note} — £${amount}`;
+
+    setExpenses([
+      ...expenses,
+      newExpense
+    ]);
+
+    setAmount("");
+    setNote("");
+  }
+
+  // Calculate Total Spending
+  const total =
+    expenses.reduce((sum, item) => {
+
+      const price =
+        Number(item.split("£")[1]) || 0;
+
+      return sum + price;
+
+    }, 0);
+
+  return (
+
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        padding: 20
+      }}
+    >
+
+      <Text
+        style={{
+          fontSize: 30,
+          textAlign: "center",
+          marginBottom: 20
+        }}
+      >
+        PocketFlow
+      </Text>
+
+      <Text>Amount</Text>
+
+      <TextInput
+        value={amount}
+        onChangeText={setAmount}
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          marginBottom: 20
+        }}
+      />
+
+      <Text>Note</Text>
+
+      <TextInput
+        value={note}
+        onChangeText={setNote}
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          marginBottom: 20
+        }}
+      />
+
+      <Button
+        title="Add Expense"
+        onPress={addExpense}
+      />
+
+      {expenses.map((item, index) => (
+
+        <Text
+          key={index}
+          style={{
+            marginTop: 20,
+            fontSize: 22
+          }}
+        >
+          {item}
+        </Text>
+
+      ))}
+
+      <Text
+        style={{
+          marginTop: 30,
+          fontSize: 28,
+          fontWeight: "bold"
+        }}
+      >
+        Total: £{total.toFixed(2)}
+      </Text>
+
+    </View>
+
+  );
+}
